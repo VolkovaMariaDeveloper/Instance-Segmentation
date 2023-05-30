10739142b9a7423a1b31051b4561447a5b07d559
catsloveme <volkovadeveloper@gmail.com>
2023-05-31 00:11:58 +0300

    многопоточность

--- source/model/segmentation/CondInst.py
+++ source/model/segmentation/CondInst.py
@@ -1,4 +1,10 @@
 from source.model.segmentation.ISegmentation import ISegmentation
+import subprocess 
+import threading as th
+import time
+from subprocess import PIPE, run
+import warnings
+import os
 
 
 class CondInst(ISegmentation):
@@ -6,9 +12,10 @@ class CondInst(ISegmentation):
     OUTPUT_PATH = "~/video/output/CondInst"
     MODEL_WEIGHTS = "training_dir/CondInst_MS_R_50_BiFPN_3x_sem.pth"
     CONFIDENCE_THRESHOLD = 0.35
-    def __init__(self):
+    def __init__(self,videoPath):
         self.name = "CondInst"
         self.argumentsDictionary ={} #возможно его следует создавать не здесь
+        self.videoPath = videoPath
     def segmentation(self, videoPath):
         self.argumentsDictionary["video_input"] = videoPath
         self.argumentsDictionary["config_file"] = self.CONFIG_FILE
@@ -20,6 +27,46 @@ class CondInst(ISegmentation):
 
 
         pass
-    def test(self,videoPath):
-        return "/home/mary/video/output/BlendMask/kiss.mkv"
+    def cmd(self):
 
+        comand = "python ~/application/Instance-Segmentation/AdelaiDet/demo/demo.py \
+    --config-file ~/application/Instance-Segmentation/AdelaiDet/configs/CondInst/MS_R_50_BiFPN_3x_sem.yaml \
+    --video-input " + self.videoPath + " \
+    --output ~/application/Instance-Segmentation/data/output/CondInst \
+    --confidence-threshold 0.35 \
+    --opts MODEL.WEIGHTS ~/application/Instance-Segmentation/AdelaiDet/training_dir/CondInst_MS_R_50_BiFPN_3x_sem.pth "
+        activateEnv = '. $CONDA_PREFIX/etc/profile.d/conda.sh && conda activate adelai-det &&'
+        with open("/home/mary/application/Instance-Segmentation/source/test.log", "w+") as file:
+            result = subprocess.run(activateEnv + comand,stdout=PIPE, stderr=file, universal_newlines=True, shell = True)
+
+    def parsingString(self,string):
+        str = ""
+        for i in string:
+            if (i =="%"):
+                return str
+            else:
+                str+=i
+
+
+    def printpipe(self):
+        persent = ""
+        while (persent!="100"):
+            with open("/home/mary/application/Instance-Segmentation/source/test.log", "r") as file:
+                #fillFile = file.read()
+                persent = self.parsingString(file.readlines()[-1])
+                print(persent)#.decode('cp1251'))
+                time.sleep(1)
+
+
+    def test(self,videoPath):
+        
+        threads = []
+        threads.append(th.Thread(target=self.cmd))
+        threads.append(th.Thread(target=self.printpipe))
+ 
+        for i in threads:
+            i.start()
+            time.sleep(3)
+        #print(result.stderr)
+       # print(result.stdout)
+        
